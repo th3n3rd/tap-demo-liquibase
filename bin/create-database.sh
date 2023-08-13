ca5557eaf39255c2d19811741bd05486bb385734
Marco Garofalo <mgarofalo@vmware.com>
2023-08-13 11:48:41 +0100

    Fix database creation script

--- bin/create-database.sh
+++ bin/create-database.sh
@@ -4,6 +4,6 @@ set -e
 
 NAME=tap-demo-liquibase-db
 
-tanzu services claass-claim get "$NAME" || tanzu services class-claim \
+tanzu services class-claim get "$NAME" || tanzu services class-claim \
     create "$NAME" \
     --class postgresql-unmanaged
