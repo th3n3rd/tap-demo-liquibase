c824394aaca50c88bb247c6f9b1e950f19fffae8
Marco Garofalo <mgarofalo@vmware.com>
2023-08-14 17:01:37 +0100

    Fix database port-forward script in order to work on different tap sandbox clusters

--- bin/database-portforward.sh
+++ bin/database-portforward.sh
@@ -2,26 +2,25 @@
 
 set -e
 
-NS=tap-demo-liquibase-db-qvfrb
+BINDING_NAME=$(kubectl get classclaim tap-demo-liquibase-db -o yaml | yq '.status.binding.name' | tr -d '\n')
 
 echo -n "Username: "
-kubectl -n $NS \
-    get secret tap-demo-liquibase-db-qvfrb \
+kubectl get secret "$BINDING_NAME" \
     -o yaml | yq '.data.username' | base64 -d
 
 echo
 echo -n "Password: "
-kubectl -n $NS \
-    get secret tap-demo-liquibase-db-qvfrb \
+kubectl get secret "$BINDING_NAME" \
     -o yaml | yq '.data.password' | base64 -d
 
 echo
 echo -n "Database: "
-kubectl -n $NS \
-    get secret tap-demo-liquibase-db-qvfrb \
+kubectl get secret "$BINDING_NAME" \
     -o yaml | yq '.data.database' | base64 -d
 
+REFERENCE_NAME=$(kubectl get secret "$BINDING_NAME" -o yaml | yq '.metadata.ownerReferences[0].name' | tr -d '\n')
+
 echo
-kubectl -n $NS \
-    port-forward svc/tap-demo-liquibase-db-qvfrb \
+kubectl -n "$REFERENCE_NAME" \
+    port-forward "svc/$REFERENCE_NAME" \
     5432:5432
